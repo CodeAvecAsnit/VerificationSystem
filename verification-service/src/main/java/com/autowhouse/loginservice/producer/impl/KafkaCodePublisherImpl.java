302137468e4609d854ac82ced269c18e78c2109f
CodeAvecAsnit <asnitbakhati@gmail.com>
2026-03-18 17:58:46 +0545

    all of other services

--- verification-service/src/main/java/com/autowhouse/loginservice/producer/impl/KafkaCodePublisherImpl.java
+++ verification-service/src/main/java/com/autowhouse/loginservice/producer/impl/KafkaCodePublisherImpl.java
@@ -1,4 +1,4 @@
-package com.autowhouse.loginservice.service.kafka.impl;
+package com.autowhouse.loginservice.producers.impl;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
